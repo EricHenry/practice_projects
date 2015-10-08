e31f1d3f9fb0bf6a0d806586522b669cb0933423
Eric Correia <correia.eh@gmail.com>
2015-10-08 16:09:17 -0400

    created a distribution of the nester module and installed it locally.

---
+++ python/test1/head_first/chapter2/nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name			= 'nester',
+	version			= '1.0.0',
+	py_modules		= ['nester'],
+	author			= 'CoreDev',
+	author_email	= 'correia.eh@gmail.com',
+	url				= 'workingOnIt',
+	description		= 'A simple printer of nested lists',
+	);
