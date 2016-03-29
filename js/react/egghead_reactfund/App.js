64631dfcca408f58ba34e9ba0cf6b45ab5e7de23
Eric Henry Correia <ehcorreia92@gmail.com>
2016-03-29 15:37:33 -0400

    React Fundamentals -> vid 2-4

--- js/react/egghead_reactfund/App.js
+++ js/react/egghead_reactfund/App.js
@@ -1,11 +1,39 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
+/**
+ * Must return one node from render.
+ */
 class App extends React.Component {
     render() {
         return (
-            <div> Hi World </div>
+            <div>
+                <h1>Hi World</h1>
+                <b> {this.props.txt} </b>
+            </div>
         )
     }
 }
 
-export default App;
+App.propTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+
+/**
+ * Default property values
+ */
+App.defaultProps = {
+    txt: "this is the default text"
+}
+
+/**
+ * How To build Stateless components that just render onto the screen
+ */
+// const App = () => (
+//     <h1> Hello Dummies!</h1>
+// );
+
+// export default App;
+
+ReactDOM.render(<App cat={5} txt="this is the props text" />, document.getElementById("app"));
