93f534faf53450d8dd8929d8de347938f046a0af
Eric Henry Correia <ehcorreia92@gmail.com>
2016-03-29 10:43:53 -0400

    rename folder

--- js/react/egghead_reactfund/app/app.js
+++
@@ -1,6 +0,0 @@
-const React = require("react");
-const ReactDOM = require('react-dom');
-const { Router } = require('react-router');
-const routes = require('./config/routes');
-
-ReactDOM.render(<Router>{routes}</Router>, document.getElementById("app"));
