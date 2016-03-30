5ab6c0401d5dd888d4eefa8f3c95a5958fc3d24d
Eric Henry Correia <ehcorreia92@gmail.com>
2016-03-30 16:36:57 -0400

    React Fundamentals -> vid 14

--- js/react/egghead_reactfund/App.js
+++ js/react/egghead_reactfund/App.js
@@ -8,84 +8,42 @@ class App extends React.Component {
     //constructor gives us our context for this within our component
     constructor(){
         super();
-        console.log(`this value after super call`, this);
-        this.state = {
-            red: 0
-        };
-        console.log(`this value after state`, this);
-
-        // shortcut to force this.update to always have this "this" in its execution context
-        this.update = this.update.bind(this);
-    }
-
-    /**
-     * Custom Method that takes an event object
-     */
-    update(e) {
-        // setState() is part of ReactComponent API
-        this.setState({
-            red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
-        })
-    }
-
+        this.state = {data: [
+          {id: 1, name: "Simon Bailey"},{id: 2, name: "Thomas Burleson"},
+          {id: 3, name: "Will Button"},{id: 4, name: "Ben Clinkinbeard"},
+          {id: 5, name: "Kent Dodds"},{id: 6, name: "Trevor Ewen"},
+          {id: 7, name: "Aaron Frost"},{id: 8, name: "Joel Hooks"},
+          {id: 9, name: "Jafar Husain"},{id: 10, name: "Tim Kindberg"},
+          {id: 11, name: "John Lindquist"},
+          {id: 12, name: "Joe Maddalone"},
+          {id: 13, name: "Tyler McGinnis"},{id: 14, name: "Scott Moss"},
+          {id: 15, name: "Robert Penner"},{id: 16, name: "Keith Peters"},
+          {id: 17, name: "Lukas Ruebbelke"},
+          {id: 18, name: "Brett Shollenberger"}
+        ]};
+     }
     /**
      * Must return one node from render().
      */
     render() {
+        let rows = this.state.data.map( person => {
+            return <PersonRow key={person.id} data={person} />
+        })
         return (
-            <div>
-                <NumInput
-                    ref="red"
-                    min={0}
-                    max={255}
-                    step={1}
-                    val={+this.state.red}
-                    label="Red"
-                    update={this.update} />
-                {this.state.red}
-            </div>
+            <table>
+                <tbody>{rows}</tbody>
+            </table>
         )
     }
 }
 
-class NumInput extends React.Component {
-    render() {
-        let label = this.props.label !== `` ?
-            <label>{this.props.label} - {this.props.val}</label> : ``;
-        return (
-            <div>
-                <input ref="inp"
-                    type={this.props.type}
-                    min={this.props.min}
-                    max={this.props.max}
-                    step={this.props.step}
-                    defaultValue={this.props.val}
-                    onChange={this.props.update} />
-                {label}
-            </div>
-        )
-    }
-}
-
-const { number, string, func, oneOf } = React.PropTypes;
-
-NumInput.propTypes ={
-    min: number,
-    max: number,
-    step: number,
-    val: number,
-    label: string,
-    update: func.isRequired,
-    type: oneOf(["number", "range"])
-};
-
-NumInput.defaultProps = {
-    min: 0,
-    max: 0,
-    step: 1,
-    val: 0,
-    label: ``,
-    type: `range`
+const PersonRow = (props) => {
+    return (
+        <tr>
+            <td>{props.data.id}</td>
+            <td>{props.data.name}</td>
+        </tr>
+    )
 }
 
 export default App;
